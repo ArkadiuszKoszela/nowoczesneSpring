b206abeeae06f3c4d9435efda3f25e6691e6a6f2
Arkadiusz Koszela <arkadiusz.koszela1@gmail.com>
2020-04-22 19:20:39 +0200

    Initial commit

---
+++ src/main/java/pl/koszela/nowoczesnebud/NowoczesneBudApplication.java
@@ -0,0 +1,13 @@
+package pl.koszela.nowoczesnebud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowoczesneBudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NowoczesneBudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/koszela/nowoczesnebud/NowoczesneBudApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koszela.nowoczesnebud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NowoczesneBudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
